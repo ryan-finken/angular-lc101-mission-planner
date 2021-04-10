b13bdca5fcf1e882360304ecef08d13d95105768
Ryan Finken <ryanfinken338@gmail.com>
2021-04-10 16:51:08 -0500

    finished experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-   experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
+  experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
+  plannedExperiments: string[] = [];
+  constructor() { }
 
-   constructor() { }
+  ngOnInit() { }
 
-   ngOnInit() { }
+  planExperiment(experiment: string): void {
+    if (this.plannedExperiments.length < 3) {
+      this.plannedExperiments.push(experiment);
+      let index = this.experiments.indexOf(experiment);
+      this.experiments.splice(index, 1);
+    }
+  }
+
+  removeExperiment(experiment: string): void {
+    this.experiments.push(experiment);
+    let index = this.plannedExperiments.indexOf(experiment);
+    this.plannedExperiments.splice(index, 1);
+  }
 
 }
