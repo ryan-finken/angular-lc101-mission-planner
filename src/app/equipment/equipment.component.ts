dff42484bc7bd205ea3b1cb7ac3090e82ed83c61
Ryan Finken <ryanfinken338@gmail.com>
2021-04-10 16:26:31 -0500

    finished equipment bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -11,7 +11,7 @@ interface EquipmentItem {
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-  equipmentItems: object[] = [
+  equipmentItems: EquipmentItem[] = [
     { name: 'Duct Tape', mass: 0.5 },
     { name: 'Space Camera', mass: 20 },
     { name: 'Food', mass: 150 },
@@ -22,7 +22,7 @@ export class EquipmentComponent implements OnInit {
     { name: 'Satellite', mass: 1200 },
     { name: 'R2 Unit', mass: 32 }
   ];
-  cargoHold: object[] = [];
+  cargoHold: EquipmentItem[] = [];
   cargoMass: number = 0;
   maximumAllowedMass: number = 2000;
   maxItems: number = 10;
@@ -42,5 +42,31 @@ export class EquipmentComponent implements OnInit {
   emptyCargoHold(): void {
     this.cargoHold = [];
     this.cargoMass = 0;
+  }
+
+  itemCountInHold(item: EquipmentItem): number {
+    let numOfItems: number = 0;
+    for (const equipment of this.cargoHold) {
+      if (equipment.name === item.name) {
+        numOfItems++;
+      }
+    }
+    return numOfItems;
+  }
+
+  getItemIndex(item: EquipmentItem): number {
+    let itemIndex: number = -1; // assume item not in cargo hold
+    for (let i = 0; i < this.cargoHold.length; i++) {
+      if (this.cargoHold[i].name === item.name) {
+        itemIndex = i;
+        break;
+      }
+    }
+    return itemIndex;
+  }
+
+  removeItem(item: EquipmentItem): void {
+    this.cargoHold.splice(this.getItemIndex(item), 1);
+    this.cargoMass -= item.mass;
   }
 }
